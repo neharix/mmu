f02990c206525881a204975c2a9db84fd7c739f9
neharix <142607660+neharix@users.noreply.github.com>
2025-04-17 17:01:01 +0500

    update

--- dist/assets/Page403-B8JukmQS.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./TheError-4GLLCFER.js";import{d as a,c as s}from"./index-DEayMjU6.js";const m={__name:"Page403",setup(t){return(o,r)=>(s(),a(e,{"status-code":403,message:"rugsat ýok","is-home-link-enabled":!0}))}};export{m as default};
--- dist/assets/Page404-D2i9ptiy.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./TheError-4GLLCFER.js";import{d as e,c as s}from"./index-DEayMjU6.js";const m={__name:"Page404",setup(t){return(o,r)=>(s(),e(a,{"status-code":404,message:"sahypa tapylmady","is-home-link-enabled":!0}))}};export{m as default};
